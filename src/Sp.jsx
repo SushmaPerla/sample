082b111d6c65607c1123c5b67961425d48ab35f5
Sushma Perla <sushmaperla2003@gmail.com>
2024-03-21 11:28:35 +0530

    added sp file

---
+++ src/Sp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sp = () => {
+  return <div>Sp</div>;
+};
+
+export default Sp;
---
+++ src/Sushma.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sushma = () => {
+  return <div>Sushma</div>;
+};
+
+export default Sushma;
